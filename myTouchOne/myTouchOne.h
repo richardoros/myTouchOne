7f6d4b9b295be1b8f5ecadbda2af631ada88ee5e
richardoros <richardoros@richards-macbook-pro.local>
2017-03-20 15:21:49 +0100

    Initial Commit

---
+++ myTouchOne/myTouchOne.h
@@ -0,0 +1,19 @@
+//
+//  myTouchOne.h
+//  myTouchOne
+//
+//  Created by Richard Oros on 20/03/2017.
+//  Copyright © 2017 Richard Oros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for myTouchOne.
+FOUNDATION_EXPORT double myTouchOneVersionNumber;
+
+//! Project version string for myTouchOne.
+FOUNDATION_EXPORT const unsigned char myTouchOneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myTouchOne/PublicHeader.h>
+
+
