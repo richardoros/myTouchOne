6be8e968b07abf3862b371ad5219c846432a0006
richardoros <richardoros@Richards-MacBook-Pro.local>
2017-03-24 14:36:03 +0100

    First Commit

---
+++ myTouchOne/AppDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  AppDelegate.swift
+//  myTouchOne
+//
+//  Created by Richard Oros on 20/03/2017.
+//  Copyright © 2017 Richard Oros. All rights reserved.
+//
+
+import Foundation
--- myTouchOne/myTouchOne.h
+++ myTouchOne/myTouchOne.h
@@ -7,6 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+import Firebase
+
+
 
 //! Project version number for myTouchOne.
 FOUNDATION_EXPORT double myTouchOneVersionNumber;
